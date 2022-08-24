2243aebf68f29c0575890c9268414bf975cd4d0d
Sabbir <webexpertsabbir@gmail.com>
2022-08-24 19:31:05 +0600

    arry dublicate

--- js/spread.js
+++ js/spread.js
@@ -5,7 +5,8 @@ const number = [20, 100, 30, 90, 99];
 const largest = Math.max(...number);
 // console.log(largest);
 
-const numbers = number;
+const numbers = [40, 40, 100, ...number, 4000];
+// const numbers = [...number];
 numbers.push(444);
 console.log(numbers);
 console.log(number)
