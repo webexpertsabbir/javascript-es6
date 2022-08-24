d3b5f1fb022b4fc15d31006bcf41ab164c0425a4
Sabbir <webexpertsabbir@gmail.com>
2022-08-24 10:02:57 +0600

    dynamic litarel

--- js/tamplate.js
+++ js/tamplate.js
@@ -4,4 +4,12 @@ const maltiply = `Amar sonar Bangle ami
 tomay valobasxi
 chirodin tomar akash`;
 
-console.log(maltiply);
+// console.log(maltiply);
+
+const a = 20;
+const b = 30;
+
+const number = [20, 30, 50, 40, 10];
+
+const summary = `Some of ${a} and ${number.length} is : ${a + b}`;
+console.log(summary);
