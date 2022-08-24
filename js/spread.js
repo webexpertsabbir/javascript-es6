02250009b1358f2fb3776ab43600a309e0457ad8
Sabbir <webexpertsabbir@gmail.com>
2022-08-24 19:24:04 +0600

    push

---
+++ js/spread.js
@@ -0,0 +1,11 @@
+const max = Math.max(20, 39, 100);
+// console.log(max);
+
+const number = [20, 100, 30, 90, 99];
+const largest = Math.max(...number);
+// console.log(largest);
+
+const numbers = number;
+numbers.push(444);
+console.log(numbers);
+console.log(number)
