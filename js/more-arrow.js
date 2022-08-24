619269b83d666590f031e187d2c4c534d92a5438
Sabbir <webexpertsabbir@gmail.com>
2022-08-24 10:57:12 +0600

    now paramiter arrow function

--- js/more-arrow.js
+++ js/more-arrow.js
@@ -2,3 +2,19 @@ const maltiply = (a, b) => a * b;
 
 const total = maltiply(10, 10);
 console.log(total);
+
+const fullName = (first, second) => first + ' ' + second;
+
+const addAll = (a, b, c, d, e, ) => a + b + c + d + e;
+
+// now paramiter arrow fuction
+
+const getPi = () => 3.14;
+
+// no paramiter
+const doubleIt = (num) => num * 2;
+
+// no paramiter simple virsion
+const fiveTime = num => num * 5;
+
+
